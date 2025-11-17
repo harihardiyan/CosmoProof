9fdec4c9a0fe8a7136df0b4411cf9c7da82f9421
harihardiyan <lorozloraz@gmail.com>
2025-11-18 02:45:48 +0700

    Create Verifier.sol

---
+++ contracts/Verifier.sol
@@ -0,0 +1,15 @@
+// contracts/Verifier.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17; 
+/* NOTE: Ini adalah placeholder. File ini akan ditimpa oleh snarkjs setelah menjalankan run_me.sh. */
+contract Verifier { 
+    function verifyProof( 
+        uint[2] memory a, 
+        uint[2][2] memory b, 
+        uint[2] memory c, 
+        uint[] memory input 
+    ) public pure returns (bool) { 
+        // Logika verifikasi akan diganti oleh snarkjs.
+        return true; 
+    } 
+}
